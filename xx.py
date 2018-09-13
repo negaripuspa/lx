4dabcb43095b6ac315d0386df1a65c1a7d411175
negaripuspa <42990046+negaripuspa@users.noreply.github.com>
2018-09-13 11:17:20 +0700

    Update xx.py

--- xx.py
+++ xx.py
@@ -11,3 +11,57 @@ def mengetik(s):
 #angka terkecil adalah yang paling cepat
 #angka terbesar adalah yang paling lambat
 mengetik('test\nbro\ntest.')
+# -*- coding: utf-8 -*-
+import os
+import sys
+import platform
+import time
+from xlpy import *
+import base64
+
+g = "\033[32;1m"
+gt = "\033[0;32m"
+bt = "\033[34;1m"
+b = "\033[36;1m"
+m = "\033[31;1m"
+c = "\033[0m"
+p = "\033[37;1m"
+u = "\033[35;1m"
+M = "\033[3;1m"
+k = "\033[33;1m"
+kt = "\033[0;33m"
+a = "\033[30;1m"
+
+W = '\x1b[0m'
+R = '\x1b[31m'
+G = '\x1b[1;32m'
+O = '\x1b[33m'
+B = '\x1b[34m'
+P = '\x1b[35m'
+C = '\x1b[36m'
+GR = '\x1b[37m'
+
+
+
+def slowprints(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(2.0/90)
+def lodprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(7.0/90)
+
+luna=(gt+"""
+#       #     # #     #  ##### 
+#       #     # ##    # #     # 
+#       #     # # #   # #     # 
+#       #     # #  #  # #######    
+#       #     # #   # # #     # 
+#       #     # #     # #     # 
+ #####   #####  #     # #     #   
+===============================
+""")
+l="tunggu ya bro.."
